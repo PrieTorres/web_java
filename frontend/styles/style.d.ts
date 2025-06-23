541d98c26173f168d8f66692e840328c61476c52
Priscila <priscila.torres@clinicorp.com>
2025-06-23 17:51:17 -0300

    up

--- frontend/styles/style.d.ts
+++ frontend/styles/style.d.ts
@@ -3,9 +3,7 @@ import "styled-components";
 import { theme } from "./theme";
 
 export type CustomTheme = typeof theme;
-
+interface DefaultTheme extends CustomTheme {}
 declare module "styled-components" {
-  export interface DefaultTheme extends CustomTheme {
-    teste: string;
-  }
+  export interface DefaultTheme extends CustomTheme {}
 }
